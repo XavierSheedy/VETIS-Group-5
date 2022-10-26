fd9ad8025019f964c585f419866aec09c383d076
TTVKiwi <113494578+TTVKiwi@users.noreply.github.com>
2022-10-26 17:01:44 +1100

    Banana package

---
+++ Assets/Karting/Scripts/megabananapealslowitem.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Graphs;
+using UnityEngine;
+using KartGame.KartSystems;
+
+public class megabananapealslowitem : MonoBehaviour
+{
+    //public float variable for the slow speed
+    public float slowSpeed = 5f;
+
+    public ArcadeKart arcadeKart;
+
+    private void Start()
+    {
+        arcadeKart = GetComponent<ArcadeKart>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("Hit");
+        if(col.gameObject.name == "harrison banana peal model final")
+        {
+            //use getcomponent to save the Arkadekart script in the Arkadekart variable
+            arcadeKart.baseStats.TopSpeed = slowSpeed;
+         
+
+
+            Debug.Log("Hit Banana peal");
+            Destroy(col.gameObject);
+        }
+    }
+   
+   
+   
+        
+   
+}
